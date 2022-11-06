6e8fd0acc4e11372befafc918a640013ad757dc9
Oliver Gregorius <98558972+olivergregorius@users.noreply.github.com>
2022-11-06 22:59:13 +0100

    #5: Added timeout parameter, optimized response closing (#9)

--- tests/mocks/micropython_ota_mock.py
+++ tests/mocks/micropython_ota_mock.py
@@ -1,5 +1,6 @@
-def mock_check_version_true(host, project):
+def mock_check_version_true(host, project, timeout):
     return True, 'v1.0.1'
 
-def mock_check_version_false(host, project):
+
+def mock_check_version_false(host, project, timeout):
     return False, 'v1.0.1'
