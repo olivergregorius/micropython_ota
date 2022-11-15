d80f868984978364eb77b7d5a61651058b41efba
Oliver Gregorius <oliver@gregorius.dev>
2022-11-15 23:45:26 +0100

    Replayed changes from LexxM3

--- micropython_ota.py
+++ micropython_ota.py
@@ -1,16 +1,21 @@
 import machine
 import urequests
 import uos
+import ubinascii
 
 
-def check_version(host, project, timeout=5):
+def check_version(host, project, auth=None, timeout=5) -> (bool, str):
     try:
         if 'version' in uos.listdir():
             with open('version', 'r') as current_version_file:
                 current_version = current_version_file.readline().strip()
         else:
             current_version = ''
-        response = urequests.get(f'{host}/{project}/version', timeout=timeout)
+
+        if auth:
+            response = urequests.get(f'{host}/{project}/version', headers={'Authorization': f'Basic {auth}'}, timeout=timeout)
+        else:
+            response = urequests.get(f'{host}/{project}/version', timeout=timeout)
         response_status_code = response.status_code
         response_text = response.text
         response.close()
@@ -24,14 +29,27 @@ def check_version(host, project, timeout=5):
         return False, current_version
 
 
-def ota_update(host, project, filenames, use_version_prefix=True, reset_device=True, timeout=5):
+def generate_auth(user=None, passwd=None) -> str | None:
+    if not user and not passwd:
+        return None
+    if (user and not passwd) or (passwd and not user):
+        raise ValueError('Either only user or pass given. None or both are required.')
+    auth_bytes = ubinascii.b2a_base64(f'{user}:{passwd}'.encode())
+    return auth_bytes.decode().strip()
+
+
+def ota_update(host, project, filenames, use_version_prefix=True, user=None, passwd=None, reset_device=True, timeout=5) -> None:
     all_files_found = True
+    auth = generate_auth(user, passwd)
     prefix_or_path_separator = '_' if use_version_prefix else '/'
     try:
-        version_changed, remote_version = check_version(host, project, timeout=timeout)
+        version_changed, remote_version = check_version(host, project, auth=auth, timeout=timeout)
         if version_changed:
             for filename in filenames:
-                response = urequests.get(f'{host}/{project}/{remote_version}{prefix_or_path_separator}{filename}', timeout=timeout)
+                if auth:
+                    response = urequests.get(f'{host}/{project}/{remote_version}{prefix_or_path_separator}{filename}', headers={'Authorization': f'Basic {auth}'}, timeout=timeout)
+                else:
+                    response = urequests.get(f'{host}/{project}/{remote_version}{prefix_or_path_separator}{filename}', timeout=timeout)
                 response_status_code = response.status_code
                 response_text = response.text
                 response.close()
@@ -50,8 +68,9 @@ def ota_update(host, project, filenames, use_version_prefix=True, reset_device=T
         print(f'Something went wrong: {ex}')
 
 
-def check_for_ota_update(host, project, timeout=5):
-    version_changed, remote_version = check_version(host, project, timeout=timeout)
+def check_for_ota_update(host, project, user=None, passwd=None, timeout=5):
+    auth = generate_auth(user, passwd)
+    version_changed, remote_version = check_version(host, project, auth=auth, timeout=timeout)
     if version_changed:
         print(f'Found new version {remote_version}, rebooting device...')
         machine.reset()
